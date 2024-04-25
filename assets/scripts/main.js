68ac626676e2c4aa76fd719f70d3a2320110cde1
Amirreza Rashvand <rashvandamirreza19@gmail.com>
2024-04-25 15:07:47 +0330

    Add Validation On Inputs And Write Warning Message And Change Input Previous School With Date Of Birth

---
+++ assets/scripts/main.js
@@ -0,0 +1,15 @@
+const getInputs = document.querySelectorAll(".form__input-text-box");
+const getBtnSubmit = document.querySelector(".form__submit-btn");
+const getParentInput = document.querySelectorAll(".form__input");
+
+getBtnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  getInputs.forEach((input) => {
+    if (!input.value) {
+      input.parentElement.classList.add("input__error");
+    } else {
+      input.parentElement.classList.remove("input__error");
+    }
+  });
+});
